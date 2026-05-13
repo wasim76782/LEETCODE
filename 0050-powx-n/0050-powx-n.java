6cc90710cd14be922ba2e8afce1911340f3c123b
wasim76782 <wraja76782@gmail.com>
2026-05-13 12:26:20 +0530

    Time: 0 ms (100%), Space: 48.4 MB (10.9%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.java
@@ -0,0 +1,19 @@
+class Solution {
+    public double myPow(double x, int n) {
+        if(n<0){
+            n=n*-1; //make n wiil positive
+            x=1/x;
+
+        }
+        double result=1;
+        while(n!=0){   
+            if(n%2!=0){
+                result=result*x;
+            }
+            x=x*x;
+            n=n/2;
+        }
+        return result;
+
+    }
+}
