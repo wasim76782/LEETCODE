fbf99b9af4c2f96ea64a85b87cbe0135773f3229
wasim76782 <wraja76782@gmail.com>
2026-09-12 15:10:45 +0530

    Time: 0 ms (100%), Space: 42.8 MB (53.25%) - LeetHub

---
+++ 2351-first-letter-to-appear-twice/2351-first-letter-to-appear-twice.java
@@ -0,0 +1,12 @@
+class Solution {
+    public char repeatedCharacter(String s) {
+        for (int i = 0; i < s.length(); i++){
+            for (int j = 0; j < i; j++){
+                if (s.charAt(i) == s.charAt(j)){
+                    return s.charAt(i);
+                }
+            }
+        }
+        return ' ';
+    }
+}
